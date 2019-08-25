92e9248cbdc74cfa2772e138ef67a747424f7417
Fuyan777 <fuyamiwa713@gmail.com>
2019-08-25 11:03:44 +0900

    [fix]デザインの修正

--- gaya-music/emoiView.swift
+++ gaya-music/emoiView.swift
@@ -12,23 +12,36 @@ class emoiView: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var dateLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        dateLabel.text = "2018/7/13"
         scroll()
-        // Do any additional setup after loading the view.
     }
     
-    var imageWidth: CGFloat = 370
-    var imageHeight: CGFloat = 370
+    var imageWidth: CGFloat = 300
+    var imageHeight: CGFloat = 300
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        switch pageControl.currentPage {
+        case 0:
+            dateLabel.text = "2018/8/20"
+        case 1:
+            dateLabel.text = "2018/9/10"
+        case 2:
+            dateLabel.text = "2018/9/16"
+        default:
+            dateLabel.text = "2018/7/13"
+        }
+        
         pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
     }
     
     func scroll(){
         
-        scrollView.frame = CGRect(x: 25.0, y: 225.0, width: imageWidth, height: imageHeight)
+        scrollView.frame = CGRect(x: 58.0, y: 250.0, width: imageWidth, height: imageHeight)
         scrollView.contentSize = CGSize(width: imageWidth * 4, height: imageHeight)
         scrollView.isPagingEnabled = true
         
@@ -45,7 +58,7 @@ class emoiView: UIViewController, UIScrollViewDelegate {
         scrollView.addSubview(secondImageView)
         
         // 3枚目の画像
-        let thirdImageView = UIImageView(image: UIImage(named: "fuya.jpg"))
+        let thirdImageView = UIImageView(image: UIImage(named: "musicImage.png"))
         thirdImageView.frame = CGRect(x: imageWidth * 2.0, y: 0.0, width: imageWidth, height: imageHeight)
         thirdImageView.contentMode = UIView.ContentMode.scaleAspectFill
         scrollView.addSubview(thirdImageView)
